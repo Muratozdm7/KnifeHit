c54701db4e62afdb7b524215d63113903f260700
Murat Özdemir <101792030+Muratozdm7@users.noreply.github.com>
2023-06-01 11:29:31 +0300

    Assetler eklendi ve Circle Controller yazildi

---
+++ KnifeHitGame/Assets/Scripts/CircleController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CircleController : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed;
+
+
+
+    private void Update()
+    {
+        SetCircleRotate();
+    }
+
+
+    private void SetCircleRotate()
+    {
+        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+    }
+}
